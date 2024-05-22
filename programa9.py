29e3d384e9453a05369f50fa611990ae93476eec
Emiliano Holguin <emiliano_3141230033@utd.edu.mx.com>
2024-05-22 08:57:36 -0600

    Numeros Pares

---
+++ programa9.py
@@ -0,0 +1,10 @@
+
+while True:
+    contraseña = input("Ingresa la contraseña: ")
+    if contraseña == "POO123":
+        print("Contraseña Correcta")
+        break
+    else:
+        print("Contraseña Incorrecta, vuelve a intentarlo")
+        
+        
