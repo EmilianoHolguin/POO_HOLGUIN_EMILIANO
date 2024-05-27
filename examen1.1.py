4a6cbde6dd8ff5cac1e5ac1b4cbdfbed48b3fff3
Emiliano Holguin <emiliano_3141230033@utd.edu.mx.com>
2024-05-27 11:05:37 -0600

    Examen Parcial 1

---
+++ examen1.1.py
@@ -0,0 +1,11 @@
+numero=float(input("Ingresa un numero entero: \n"))
+
+if numero >= 1:
+    print(numero,"es un numero positivo...")
+elif numero <= 1:
+    print(numero, "es un numero negativo...")
+else:
+    print("Tu numero es cero")
+    
+    
+    
