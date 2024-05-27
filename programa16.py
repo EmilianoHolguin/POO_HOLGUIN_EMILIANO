580bede8f853f8735cf4261d804ea99d5a2b607f
Emiliano Holguin <emiliano_3141230033@utd.edu.mx.com>
2024-05-26 21:29:07 -0600

    Promedio mayor

---
+++ programa16.py
@@ -0,0 +1,14 @@
+a=(float(input("Ingresa el primer promedio:")))
+b=(float(input("Ingresa el Segundo promedio:")))
+c=(float(input("Ingresa el Tercer promedio:")))
+
+if a > b and a > c:
+    print("El promedio mayor es:",a)
+elif b > a and b > c:
+    print("El promedio mayor es:",b)
+elif c > a and c > b:
+    print("El promedio mayor es:",c)
+else:
+    print("Los numeros son iguales")
+
+    
