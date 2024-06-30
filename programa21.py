33dc84fcb68da63bef001b848fe0955af767812b
Emiliano Holguin <emiliano_3141230033@utd.edu.mx.com>
2024-06-30 00:22:31 -0600

    Programa 21 – Tupla desempaquetada

---
+++ programa21.py
@@ -0,0 +1,10 @@
+persona=("Emiliano", 21, "Masculino", 1.70, 59.0, "22/01/2003")
+
+nombre, edad, sexo, estatura, peso, fecha = persona
+
+print("NOMBRE:", nombre)
+print("EDAD:", edad)
+print("SEXO:", sexo)
+print("ESTATURA:", estatura,"mt")
+print("PESO:", peso,"kg")
+print("FECHA DE NACIMIENTO:", fecha)
